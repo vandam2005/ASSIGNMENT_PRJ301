524b492b666cc4aa7c7a4a64aa0b6c85e481c836
Loc-LX <locdoncare2004@gmail.com>
2026-03-23 16:50:47 +0700

    Add files via upload

---
+++ src/java/chat/HttpSessionConfigurator.java
@@ -0,0 +1,19 @@
+package controller.chat;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.HandshakeResponse;
+import jakarta.websocket.server.HandshakeRequest;
+import jakarta.websocket.server.ServerEndpointConfig;
+
+public class HttpSessionConfigurator extends ServerEndpointConfig.Configurator {
+
+    @Override
+    public void modifyHandshake(ServerEndpointConfig config,
+                                HandshakeRequest request,
+                                HandshakeResponse response) {
+        HttpSession httpSession = (HttpSession) request.getHttpSession();
+        if (httpSession != null) {
+            config.getUserProperties().put("httpSession", httpSession);
+        }
+    }
+}
